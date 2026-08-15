3a3e5c6b3063d87155772de277c1cecc5bd28176
subhammahankuda010-art <subhammahankuda010@gmail.com>
2026-08-15 12:48:42 +0530

    Initial website commit

---
+++ page5.js
@@ -0,0 +1,22 @@
+const wish = document.querySelector(".wish");
+const mainText = document.querySelector(".main-text");
+const youText = document.getElementById("youText");
+const messageBox = document.querySelector(".message-box");
+
+wish.addEventListener("click", () => {
+    wish.style.display="none";
+
+    setTimeout(() => {
+        mainText.style.display = "block";
+        youText.style.display = "block";
+    },500);
+});
+
+
+youText.addEventListener("click",()=>{
+    messageBox.classList.remove("hidden");
+
+    setTimeout(() => {
+        messageBox.classList.remove("hidden");
+    },500);
+});
